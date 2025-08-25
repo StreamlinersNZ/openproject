ce294bee5dde9109fdbb1c5d57417ea5b8efb1fa
Tobias Dillmann <github@pentokami.de>
2025-08-25 09:12:29 +0200

    [#65740] WIP pdf export spec

--- spec/models/work_packages/pdf_export/work_package_list_to_pdf_spec.rb
+++ spec/models/work_packages/pdf_export/work_package_list_to_pdf_spec.rb
@@ -225,6 +225,83 @@ RSpec.describe WorkPackage::PDFExport::WorkPackageListToPdf do
           "1/1", export_date_formatted, query.name
         ].join(" ")
       end
+
+      describe "grouped by a phase" do
+        shared_let(:project_phase) { create(:project_phase, definition: create(:project_phase_definition)) }
+        shared_let(:other_project_phase) { create(:project_phase, definition: create(:project_phase_definition)) }
+        shared_let(:phases) { [project_phase, other_project_phase] }
+        shared_let(:user_permissions) { %w[view_work_packages export_work_packages view_project_phases] }
+        shared_let(:user) { create(:admin) }
+
+        let(:column_names) { %w[id subject status project_phase] }
+        let(:query_attributes) { { group_by: "project_phase" } }
+
+        let!(:wp_with_phase) do
+          create(:work_package,
+                 project:,
+                 project_phase_definition: project_phase.definition,
+                 subject: "Work package with phase",
+                 description: "Work package with a phase")
+        end
+        let!(:other_wp_with_phase) do
+          create(:work_package,
+                 project:,
+                 project_phase_definition: project_phase.definition,
+                 subject: "Work package 2 with phase",
+                 description: "Work package 2 with a phase")
+        end
+        let!(:wp_with_other_phase) do
+          create(:work_package,
+                 project:,
+                 project_phase_definition: other_project_phase.definition,
+                 subject: "Work package with other phase",
+                 description: "Work package with another phase")
+        end
+
+        before do
+          login_as(user)
+          query.save!
+
+          described_class.new(query, options)
+          Timecop.freeze(export_time) do
+            export.export!
+          end
+        end
+
+        it "contains correct data" do
+          puts "FOOOOOOO -----------------------------------------------------------------------------------"
+          # pp project.work_packages.map { [it.subject, it.status.name] }
+          puts "Query:"
+          pp query
+          puts "Query Filters:"
+          pp query.filters
+          puts "Query results all_group_sums:"
+          pp query.results.all_group_sums
+          show
+          puts "FOOOOOOO -----------------------------------------------------------------------------------"
+
+          # FIXME: use correct expectations and fix spec once the query seems to work
+          expect(true).to eq(false)
+
+          # expect(pdf_strings).to eq [
+          #                             *cover_page_content,
+          #                             query.name,
+          #                             "1.", "2", work_package_parent.subject,
+          #                             "2.", "2", work_package_child.subject,
+          #                             "3.", "1", wp_with_phase.subject,
+          # "1/2", export_date_formatted, query.name,
+          # I18n.t("js.work_packages.tabs.overview"),
+          # column_title(:project_phaoe), column_title(:story_points), column_title(:done_ratio),
+          # work_package_parent.type.name, work_package_parent.story_points.to_s, "25%",
+          # work_package_child.type.name, work_package_child.story_points.to_s, "50%",
+          # I18n.t("js.label_sum"), work_packages_sum.to_s, "38%",
+          # *work_package_details(work_package_parent, "1", long_text_fields),
+          # *work_package_details(work_package_child, "2", long_text_fields),
+          # "2/2", export_date_formatted, query.name
+          # ].join(" ")
+        end
+
+      end
     end
 
     describe "grouped with sums" do
@@ -265,91 +342,7 @@ RSpec.describe WorkPackage::PDFExport::WorkPackageListToPdf do
       end
     end
 
-    describe "grouped by a phase with sums" do
-      shared_let(:project_phase) { create(:project_phase, definition: create(:project_phase_definition)) }
-      shared_let(:other_project_phase) { create(:project_phase, definition: create(:project_phase_definition)) }
-      shared_let(:phases) { [project_phase, other_project_phase] }
-      shared_let(:user) { create(:admin) }
 
-      let(:column_names) { %w[id subject status story_points done_ratio project_phase] }
-      let(:query_attributes) { { display_sums: true, group_by: "status" } }
-
-      let!(:wp_with_phase) do
-        create(:work_package,
-               project:,
-               project_phase_definition: project_phase.definition,
-               subject: "Work package with phase",
-               estimated_hours: 10,
-               remaining_hours: 5,
-               done_ratio: 50,
-               story_points: 2,
-               description: "Work package with a phase")
-      end
-      let!(:other_wp_with_phase) do
-        create(:work_package,
-               project:,
-               project_phase_definition: project_phase.definition,
-               subject: "Work package 2 with phase",
-               estimated_hours: 20,
-               remaining_hours: 10,
-               done_ratio: 50,
-               story_points: 6,
-               description: "Work package 2 with a phase")
-      end
-      let!(:wp_with_other_phase) do
-        create(:work_package,
-               project:,
-               project_phase_definition: other_project_phase.definition,
-               subject: "Work package with other phase",
-               estimated_hours: 10,
-               remaining_hours: 2,
-               done_ratio: 80,
-               story_points: 2,
-               description: "Work package with another phase")
-      end
-
-      before do
-        login_as(user)
-        query.save!
-
-        described_class.new(query, options)
-        Timecop.freeze(export_time) do
-          export.export!
-        end
-      end
-
-      it "contains correct data" do
-        puts "FOOOOOOO -----------------------------------------------------------------------------------"
-        # pp project.work_packages.map { [it.subject, it.status.name] }
-        puts "Query:"
-        pp query
-        puts "Query results all_group_sums:"
-        pp query.results.all_group_sums
-        show
-        puts "FOOOOOOO -----------------------------------------------------------------------------------"
-
-        # FIXME: use correct expectations and fix spec once the query seems to work
-        expect(true).to eq(false)
-
-        # expect(pdf_strings).to eq [
-        #                             *cover_page_content,
-        #                             query.name,
-        #                             "1.", "2", work_package_parent.subject,
-        #                             "2.", "2", work_package_child.subject,
-        #                             "3.", "1", wp_with_phase.subject,
-                                  # "1/2", export_date_formatted, query.name,
-                                  # I18n.t("js.work_packages.tabs.overview"),
-                                  # column_title(:project_phaoe), column_title(:story_points), column_title(:done_ratio),
-                                  # work_package_parent.type.name, work_package_parent.story_points.to_s, "25%",
-                                  # work_package_child.type.name, work_package_child.story_points.to_s, "50%",
-                                  # I18n.t("js.label_sum"), work_packages_sum.to_s, "38%",
-                                  # *work_package_details(work_package_parent, "1", long_text_fields),
-                                  # *work_package_details(work_package_child, "2", long_text_fields),
-                                  # "2/2", export_date_formatted, query.name
-                                  # ].join(" ")
-      end
-
-    end
   end
 
   context "with a request for a PDF Report" do
